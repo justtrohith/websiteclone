3589970307ee3b686dfdb863e9ddb58783f01cb6
Aman <135516605+justtrohith@users.noreply.github.com>
2024-09-11 18:34:37 +0530

    changes

--- src/components/Design.tsx
+++ src/components/Design.tsx
@@ -18,7 +18,7 @@ function Design() {
             </div>
         </div>
         <div className="text-left  max-w-[425px] mx-auto text-white">
-            <p >It's our commitment to delivering more than just services; it&#39;s about delivering results. Whether you&#39;re a startup with big dreams or an established brand looking for a fresh perspective, we&#39;re here to take your digital presence to the next level.</p>
+            <p >Its our commitment to delivering more than just services; it&#39;s about delivering results. Whether you&#39;re a startup with big dreams or an established brand looking for a fresh perspective, we&#39;re here to take your digital presence to the next level.</p>
             <p >From web design that combines form and function seamlessly to digital marketing strategies that drive growth, Digital Solutions is your partner in the digital journey. We thrive on challenges, and we&#39;re here to guide you through the ever-changing world of digital possibilities.
 
 </p>
